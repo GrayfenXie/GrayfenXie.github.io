6bef9ca3c49dd88095107263a1544de688cb7bba
GrayfenXie <915513145@qq.com>
2024-11-06 23:59:07 +0800

    1

--- js/imagearray.js
+++ js/imagearray.js
@@ -1,29 +1,3 @@
-// var imageArray = [
-//   'https://cdn.grayfen.cn/summerbbq.png',
-//   'https://cdn.grayfen.cn/Nicholas2.png',
-//   'https://cdn.grayfen.cn/cattle.png',
-//   'https://cdn.grayfen.cn/fallalone.png',
-//   'https://cdn.grayfen.cn/feelalone.png',
-//   'https://cdn.grayfen.cn/gao.png',
-//   'https://cdn.grayfen.cn/grasses.png',
-//   'https://cdn.grayfen.cn/makesnowman.png',
-//   'https://cdn.grayfen.cn/nuhcnay.png',
-//   'https://cdn.grayfen.cn/pinkspring.png',
-//   'https://cdn.grayfen.cn/ridethebike.png',
-//   'https://cdn.grayfen.cn/thelittlepark.png',
-//   'https://cdn.grayfen.cn/camera.png',
-//   'https://cdn.grayfen.cn/adventure.png',
-//   'https://cdn.grayfen.cn/rowboat.png',
-//   'https://cdn.grayfen.cn/snowmoon.png',
-//   'https://cdn.grayfen.cn/spring.png',
-//   'https://cdn.grayfen.cn/summer.png',
-//   'https://cdn.grayfen.cn/nextlife.png',
-//   'https://cdn.grayfen.cn/winter.png',
-//   'https://cdn.grayfen.cn/coolman.png',
-//   'https://cdn.grayfen.cn/blood.png',
-//   'https://cdn.grayfen.cn/smile.png',
-//   'https://cdn.grayfen.cn/umbrella.png',
-// ]
 const imagesData = [
   { src: 'https://cdn.grayfen.cn/summerbbq.png', alt: '夏日露营' },
   { src: 'https://cdn.grayfen.cn/Nicholas2.png', alt: '北极星的眼泪' },
@@ -48,8 +22,129 @@ const imagesData = [
   { src: 'https://cdn.grayfen.cn/coolman.png', alt: '帅' },
   { src: 'https://cdn.grayfen.cn/blood.png', alt: '吸血鬼' },
   { src: 'https://cdn.grayfen.cn/smile.png', alt: '微笑男孩' },
-  { src: 'https://cdn.grayfen.cn/umbrella.png', alt: '撑伞' },
+  { src: 'https://cdn.grayfen.cn/sunset.png', alt: '日落西山' },
+  { src: 'https://cdn.grayfen.cn/sunman.png', alt: '落日垂钓' },
+  { src: 'https://cdn.grayfen.cn/sunboat.png', alt: '飘向远方' },
+  { src: 'https://cdn.grayfen.cn/swordquest.png', alt: '寻剑' },
+  { src: 'https://cdn.grayfen.cn/swordsman.png', alt: '侠客行' },
+  { src: 'https://cdn.grayfen.cn/tothemoon.png', alt: '划向明月的船' },
+  { src: 'https://cdn.grayfen.cn/tothemoon2.png', alt: '随波逐流' },
+  { src: 'https://cdn.grayfen.cn/pinkwinter.png', alt: '粉色的冬天' },
 ];
+
+// // 函数用于洗牌数组
+// function shuffleArray(array) {
+//   for (let i = array.length - 1; i > 0; i--) {
+//     const j = Math.floor(Math.random() * (i + 1));
+//     [array[i], array[j]] = [array[j], array[i]];
+//   }
+// }
+
+// // 函数用于创建img元素并赋值src和alt
+// function createImageElements(imagesArray) {
+//   // 获取一个容器元素，用于存放所有的img元素
+//   const container = document.getElementById('works');
+
+//   // 遍历数组中的每个对象
+//   imagesArray.forEach(image => {
+//     // 创建一个新的li元素
+//     const li = document.createElement('li');
+
+//     // 创建一个新的img元素
+//     const img = document.createElement('img');
+//     // 为img元素设置src和alt属性
+//     img.src = image.src;
+//     img.alt = image.alt;
+
+//     // 创建一个遮罩层
+//     const overlay = document.createElement('div');
+//     overlay.className = 'image-overlay';
+//     overlay.textContent = image.alt;
+
+//     // 将img元素和遮罩层添加到li元素中
+//     li.appendChild(img);
+//     li.appendChild(overlay);
+
+//     // 将li元素添加到容器中
+//     container.appendChild(li);
+
+//     // 为img添加点击事件
+//     overlay.addEventListener('click', function () {
+//       opens(this);
+//     });
+//   });
+// }
+
+// // 调用洗牌函数
+// shuffleArray(imagesData);
+
+// // 调用函数，传入数组
+// createImageElements(imagesData);
+
+//jq动画
+window.onload = anime()
+function anime() {
+  var eles = document.getElementsByTagName('img')
+  var lxl = 1
+  for (var i = 0; i < eles.length; i++) {
+    eles[i].style.transform = 'scale(' + lxl + ')'
+  }
+}
+
+//查看原图
+var modal = document.getElementById('myModal');
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+function opens(self) {
+  modal.style.display = "block";
+  modalImg.src = self.previousElementSibling.src;
+  modalImg.alt = self.previousElementSibling.alt;
+  document.getElementById('myModal').scrollTop = 0;
+  document.onkeydown = false;
+  document.body.classList.add('no-scroll');
+  setTimeout(() => {
+    modalImg.style.opacity = 1;
+  }, "100");
+}
+var a = document.getElementsByClassName("modal-content");
+var b = document.getElementsByClassName("modal");
+var flag = true;
+function look(self) {
+  if (flag) {
+    for (var i = a.length - 1; i >= 0; i--) {
+      b[i].style.cursor = "zoom-out";
+      a[i].style.width = "100vw";
+      a[i].style.height = "auto";
+    }
+    flag = false;
+  }
+  else {
+    for (var i = a.length - 1; i >= 0; i--) {
+      b[i].style.cursor = "zoom-in";
+      a[i].style.height = "100%";
+      a[i].style.width = "auto";
+    }
+    flag = true;
+  }
+}
+// 获取 <span> 元素，设置关闭模态框按钮
+var span = document.getElementsByClassName("close")[0];
+// 点击 <span> 元素上的 (x), 关闭模态框
+span.onclick = function () {
+  modalImg.style.opacity = 0;
+  setTimeout(() => {
+    modal.style.display = "none";
+  }, "100");
+  document.body.classList.remove('no-scroll');
+  for (var i = a.length - 1; i >= 0; i--) {
+    a[i].style.height = "100%";
+    a[i].style.width = "auto";
+    b[i].style.cursor = "zoom-in";
+  }
+}
+// 当前已加载图片数量
+let loadedImages = 0;
+const imagesPerLoad = 9; // 每次加载的图片数量
 
 // 函数用于洗牌数组
 function shuffleArray(array) {
@@ -60,60 +155,45 @@ function shuffleArray(array) {
 }
 
 // 函数用于创建img元素并赋值src和alt
-function createImageElements(imagesArray) {
-  // 获取一个容器元素，用于存放所有的img元素
+function createImageElements(imagesArray, limit) {
   const container = document.getElementById('works');
+  let imagesToLoad = imagesArray.slice(loadedImages, loadedImages + limit);
 
-  // 遍历数组中的每个对象
-  imagesArray.forEach(image => {
-    // 创建一个新的li元素
+  imagesToLoad.forEach(image => {
     const li = document.createElement('li');
-    
-    // 创建一个新的img元素
     const img = document.createElement('img');
-    // 为img元素设置src和alt属性
     img.src = image.src;
     img.alt = image.alt;
-     // 创建一个遮罩层
-     const overlay = document.createElement('div');
-     overlay.className = 'image-overlay';
-     overlay.textContent = image.alt;
-     
-     // 将img元素和遮罩层添加到li元素中
-     li.appendChild(img);
-     li.appendChild(overlay);
-     
-     // 将li元素添加到容器中
-     container.appendChild(li);
+    const overlay = document.createElement('div');
+    overlay.className = 'image-overlay';
+    overlay.textContent = image.alt;
+    li.appendChild(img);
+    li.appendChild(overlay);
+    container.appendChild(li);
+    overlay.addEventListener('click', function () {
+      opens(this);
+    });
   });
+
+  loadedImages += limit;
+  if (loadedImages >= imagesArray.length) {
+    document.getElementById('load-more-btn').style.display = 'none';
+  }
+  anime();
 }
 
-// 调用洗牌函数
+// 初始化加载9张图片
 shuffleArray(imagesData);
+createImageElements(imagesData, imagesPerLoad);
 
-// 调用函数，传入数组
-createImageElements(imagesData);
+// 检查是否滚动到底部并加载更多图片
+function checkScrollPosition() {
+  const nearBottomThreshold = 10; // 当距离底部500px时开始加载
+  if (window.innerHeight + document.documentElement.scrollTop >= document.documentElement.offsetHeight - nearBottomThreshold &&
+      loadedImages < imagesData.length) {
+    createImageElements(imagesData, imagesPerLoad);
+  }
+}
 
-//jq动画
-window.onload = function () {
-   var eles = document.getElementsByTagName('img')
-   var lxl = 1
-   for (var i = 0; i < eles.length; i++) {
-     eles[i].style.transform = 'scale(' + lxl + ')'
-   }
- }
-// var newArr = []
-// while (imageArray.length != 0) {
-//   var index = Math.floor(Math.random() * imageArray.length)
-//   newArr.push(imageArray[index])
-//   imageArray.splice(index, 1)
-// }
-// const imageContainer = $('#works')
-// newArr.forEach((src) => {
-//   imageContainer.append(`
-//     <li class="works-item">
-//       <div class="works-item__text">${src.replace(pre, ' ')}</div>
-//     </li>
-//   `)
-// })
-
+// 监听滚动事件
+window.addEventListener('scroll', checkScrollPosition);
