2ed35b722eec6b06c6465c73fab598ef84a7bb0a
GrayfenXie <915513145@qq.com>
2025-09-03 22:48:58 +0800

    1

--- api/sendmail.js
+++ api/sendmail.js
@@ -1,18 +1,18 @@
 // api/sendmail.js
-const nodemailer = require('nodemailer');
+import * as nodemailer from 'nodemailer';
 
 export default async function handler(req, res) {
-  const { ip, ua } = req.query;
   const transporter = nodemailer.createTransporter({
     host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
-    secure: true,                 // 465 用 true
+    port: Number(process.env.SMTP_PORT),
+    secure: process.env.SMTP_PORT === '465',
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
     },
   });
 
+  const { ip, ua } = req.query;
   await transporter.sendMail({
     from: process.env.SMTP_USER,
     to: process.env.MAIL_TO,
